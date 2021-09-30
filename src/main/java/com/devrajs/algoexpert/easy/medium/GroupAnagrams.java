bfe49e9acd77416aeecb4c014c1b048d5f3b1f33
Devraj Singh <devrajvit@gmail.com>
2021-09-30 11:27:48 -0300

    Added solutions for two more medium questions

---
+++ src/main/java/com/devrajs/algoexpert/easy/medium/GroupAnagrams.java
@@ -0,0 +1,32 @@
+package com.devrajs.algoexpert.easy.medium;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(List<String> words) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String str : words) {
+            String sortedStr = sortString(str);
+            if(map.containsKey(sortedStr)) {
+                map.get(sortedStr).add(str);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(sortedStr, list);
+            }
+        }
+        // Write your code here.
+        List<List<String>> list =  new ArrayList<>();
+        for(Map.Entry<String, List<String>> entryMap : map.entrySet()) {
+            List<String> l = entryMap.getValue();
+            list.add(l);
+        }
+        return list;
+    }
+
+    static String sortString(String str) {
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
+        return new String(arr);
+    }
+}
